68acdecaacac058074389ee8a5fb5f250481e8a0
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2023-09-24 12:01:10 +0200

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+
+type Props = { score: number };
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "red";
+  return (
+    <Badge
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={4}
+      colorScheme={color}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
