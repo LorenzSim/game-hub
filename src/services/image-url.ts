ff84d55f0a0d635bf901ef95d1f59f7445389673
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2023-09-24 15:37:31 +0200

    added no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,3 +1,5 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const croppedImageUrl = ({
   url,
   width = 600,
@@ -7,12 +9,12 @@ const croppedImageUrl = ({
   width?: number;
   height?: number;
 }) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   const prefix = url.slice(0, index);
   const postfix = url.slice(index);
-  
+
   return `${prefix}crop/${width}/${height}/${postfix}`;
 };
 
